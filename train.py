ac2ce98367a393c42a06598119e2656dda7df027
Shane <shane@smiskol.com>
2020-02-14 07:05:17 -0600

    Delete unused code

--- train.py
+++ train.py
@@ -1,25 +1,26 @@
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+from keras import backend as K
 # K.set_floatx('float16')
 # K.set_epsilon(1e-4)
 import os
-import pickle
-import random
-import shutil
-import threading
-import time
-
+from keras.preprocessing.image import ImageDataGenerator
 import cv2
 import keras
-import matplotlib.pyplot as plt
-import numpy as np
-from keras import backend as K
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Flatten, Dense
 from keras.models import Model
 from keras.models import Sequential
-from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.layers import Activation, Dropout, Flatten, Dense
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
+import numpy as np
+import random
+import threading
+import time
+import shutil
+import pickle
 from data_generator import DataGenerator
+
 
 # config = tf.ConfigProto()
 # config.gpu_options.per_process_gpu_memory_fraction = 0.25
@@ -44,7 +45,8 @@ def show_preds():
     pred = np.argmax(pred)
 
     plt.clf()
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+    plt.imshow(traffic.BGR2RGB(img))
     plt.show()
     plt.title('Prediction: {}'.format(traffic.labels[pred]))
     plt.show()
@@ -59,7 +61,7 @@ def plot_features(layer_idx, img_idx=0, square=2):
 
     img = traffic.x_test[img_idx]
 
-    plt.imshow(cv2.cvtColor(img.astype('float32'), cv2.COLOR_BGR2RGB))
+    plt.imshow(traffic.BGR2RGB(img))
 
     feature_model = Model(inputs=traffic.model.inputs, outputs=traffic.model.layers[layer_idx].output)
     feature_maps = feature_model.predict(np.array([img]))
@@ -131,10 +133,6 @@ class TrafficLightsModel:  # TODO: USE KERAS IMAGE LOADER
         self.limit_samples = 500
 
         self.model = None
-        # self.x_train = []
-        # self.y_train = []
-        # self.x_test = []
-        # self.y_test = []
 
         self.reset_proc = force_reset
         self.classes_processed = 0
@@ -157,32 +155,6 @@ class TrafficLightsModel:  # TODO: USE KERAS IMAGE LOADER
         train_gen, valid_gen = self.get_generators()
         return train_gen, valid_gen
         # self.train_batches(train_gen, valid_gen)
-        # self.train()
-
-    def store_arrays(self):
-        for label in self.labels:
-            print('Converting {} images to arrays for faster loading...'.format(label))
-            label_path = '{}/{{}}/{}'.format(self.proc_folder, label)
-            for image in os.listdir(label_path.format('.train')):
-                img_path = '{}/{}'.format(label_path.format('.train'), image)
-                img_array = cv2.imread(img_path).astype(np.float32) / 255.
-                new_img_path = label_path.format('.train_pickle')
-                new_img_name = '.'.join(image.split('/')[-1].split('.')[:-1])
-                new_img_path = '{}/{}.p'.format(new_img_path, new_img_name)
-                h5f = h5py.File(new_img_path, 'w')
-                h5f.create_dataset('dataset1', data=img_array)
-                # np.savez_compressed(new_img_path, *[img_array for _ in range(128)])
-                print('done')
-                # with open(new_img_path, 'wb') as f:
-                #     pickle.dump(img_array, f)
-            for image in os.listdir(label_path.format('.validation')):
-                img_path = '{}/{}'.format(label_path.format('.validation'), image)
-                img_array = cv2.imread(img_path).astype(np.float32) / 255.
-                new_img_path = label_path.format('.validation_pickle')
-                new_img_name = '.'.join(image.split('/')[-1].split('.')[:-1])
-                new_img_path = '{}/{}.p'.format(new_img_path, new_img_name)
-                with open(new_img_path, 'wb') as f:
-                    pickle.dump(img_array, f)
 
 
     def train_batches(self, train_generator, valid_generator, restart=False):
@@ -198,32 +170,10 @@ class TrafficLightsModel:  # TODO: USE KERAS IMAGE LOADER
                            optimizer=opt,
                            metrics=['accuracy'])
 
-        # train_steps = int(np.ceil(self.num_train / self.batch_size))  # calculated by keras with datagenerator now
-        # valid_steps = int(np.ceil(self.num_valid / self.batch_size))
-
         self.model.fit_generator(train_generator,
                                  epochs=100,
                                  validation_data=valid_generator,
                                  workers=8)
-
-
-    # def train(self):
-    #     if self.model is None:
-    #         self.model = self.get_model()
-    #     # opt = keras.optimizers.RMSprop()
-    #     # opt = keras.optimizers.Adadelta()
-    #     # opt = keras.optimizers.Adagrad()
-    #     opt = keras.optimizers.Adam()
-    #
-    #     self.model.compile(loss='categorical_crossentropy',
-    #                        optimizer=opt,
-    #                        metrics=['accuracy'])
-    #
-    #     self.model.fit(self.x_train, self.y_train,
-    #                    epochs=100,
-    #                    batch_size=self.batch_size,
-    #                    validation_data=(self.x_test, self.y_test))
-
 
     def get_model(self):
         kernel_size = (2, 2)  # (3, 3)
@@ -248,9 +198,8 @@ class TrafficLightsModel:  # TODO: USE KERAS IMAGE LOADER
 
         return model
 
-    def RGB2BGR(self, arr):  # easier to inference on, EON uses BGR images
-        rgb = cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
-        return K.cast_to_floatx(rgb)
+    def BGR2RGB(self, arr):  # easier to inference on, EON uses BGR images
+        return cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
 
     def get_generators(self):
         train_dir = '{}/.train'.format(self.proc_folder)
@@ -258,50 +207,6 @@ class TrafficLightsModel:  # TODO: USE KERAS IMAGE LOADER
         train_generator = DataGenerator(train_dir, self.labels, self.batch_size)  # keeps data in BGR format and normalizes
         valid_generator = DataGenerator(valid_dir, self.labels, self.batch_size)
         return train_generator, valid_generator
-
-    # def get_generators(self):
-    #     train_dir = '{}/.train'.format(self.proc_folder)
-    #     valid_dir = '{}/.validation'.format(self.proc_folder)
-    #     train_datagen = ImageDataGenerator(rescale=1./255)  # modified line 1248 to convert RGB to BGR
-    #     valid_datagen = ImageDataGenerator(rescale=1./255)
-    #
-    #     train_generator = train_datagen.flow_from_directory(
-    #         train_dir,
-    #         target_size=(self.y_hood_crop, self.W),  # height, width
-    #         batch_size=self.batch_size,
-    #         class_mode='categorical')
-    #
-    #     valid_generator = valid_datagen.flow_from_directory(
-    #         valid_dir,
-    #         target_size=(self.y_hood_crop, self.W),
-    #         batch_size=self.batch_size,
-    #         class_mode='categorical')
-    #     return train_generator, valid_generator
-
-    # def load_imgs(self):
-    #     print('Loading data...', flush=True)
-    #     for image_class, phot_dir in enumerate(self.classes):
-    #         t = 0
-    #         images = os.listdir('{}/{}'.format(self.proc_folder, phot_dir))
-    #         for idx, phot in enumerate(images):
-    #             if time.time() - t >= 5:
-    #                 print('{}: Loading photo {} of {}.'.format(self.classes[image_class], idx + 1, len(images)))
-    #                 t = time.time()
-    #             img = cv2.imread('{}/{}/{}'.format(self.proc_folder, phot_dir, phot))
-    #             # img = cv2.resize(img, dsize=(self.W // self.reduction, self.H // self.reduction), interpolation=cv2.INTER_CUBIC)  # don't resize
-    #             try:
-    #                 img = img.astype(np.float16) / 255  # normalize
-    #                 self.x_train.append(img)
-    #                 self.y_train.append(self.one_hot(image_class))
-    #             except Exception as e:
-    #                 print(e)
-    #
-    #     print('\nGetting train and test set, please wait...', flush=True)
-    #     self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.x_train, self.y_train, test_size=self.test_percentage)
-    #     self.x_train = np.array(self.x_train)
-    #     self.y_train = np.array(self.y_train)
-    #     self.x_test = np.array(self.x_test)
-    #     self.y_test = np.array(self.y_test)
 
     def create_val_images(self):
         print('Separating validation images!', flush=True)
@@ -350,7 +255,6 @@ class TrafficLightsModel:  # TODO: USE KERAS IMAGE LOADER
             print('{}: Finished!'.format(image_class))
         else:
             print('All finished!')
-
 
     def process_images(self):
         datagen = ImageDataGenerator(
